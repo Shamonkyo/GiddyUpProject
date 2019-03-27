f7f7d0f71bf02ee33cc0dfe6d7224bdf3155b149
Shamonkyo <duransamuda@gmail.com>
2019-03-27 11:48:02 -0400

    most current version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthComponent} from './auth/auth.component';
+import {UserdashComponent} from './userdash/userdash.component';
+import {HomepageComponent} from './welcome/homepage/homepage.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [{
+  path: 'profile',
+  component: UserdashComponent
+},
+  {
+    path: 'login',
+    component: AuthComponent
+  },
+  { path: '',
+    component: HomepageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { UserdashComponent } from './userdash/userdash.component';
+import {AmplifyAngularModule, AmplifyService} from 'aws-amplify-angular';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import { HeaderComponent } from './navigation/header/header.component';
+
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { HomepageComponent } from './welcome/homepage/homepage.component';
+import { ProfileinfoComponent } from './userprofile/profileinfo/profileinfo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    UserdashComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    HomepageComponent,
+    ProfileinfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAngularModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
